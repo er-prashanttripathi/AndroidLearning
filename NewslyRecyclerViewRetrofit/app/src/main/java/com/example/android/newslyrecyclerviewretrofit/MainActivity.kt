dec7d9cf1027792a53df2dcdbeb423f2eddc4659
prashant <tripathiprashant741.com>
2023-01-25 15:05:54 +0530

    cheezycode retrofit api hit calling in log

--- NewslyRecyclerViewRetrofit/app/src/main/java/com/example/android/newslyrecyclerviewretrofit/MainActivity.kt
+++ NewslyRecyclerViewRetrofit/app/src/main/java/com/example/android/newslyrecyclerviewretrofit/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.android.newslyrecyclerviewretrofit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getNews()
+    }
+
+    private fun getNews() {
+        val news = NewsService.newsInstance.headlines("in", 1)
+        news.enqueue(object : Callback<News> {
+            override fun onResponse(call: Call<News>, response: Response<News>) {
+                val news = response.body()
+                if (news != null) {
+                    Log.d("Api_Calling", news.toString())
+                }
+            }
+
+            override fun onFailure(call: Call<News>, t: Throwable) {
+                Log.d("Api_Calling", "onFailure: ERROR IN FETCHING NEWS", t)
+            }
+        })
     }
 }
